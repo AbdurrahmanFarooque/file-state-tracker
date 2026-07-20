18dc4119e5b36be0cab625b9804c133cba651861
Abdurrahman Farooque <abdurrahman.f24@gmail.com>
2026-07-20 16:09:12 +0530

    Forgot to merge last time

--- models.py
+++ models.py
@@ -8,27 +8,3 @@ class CaseFile(SQLModel, table=True):
     state: str = Field(default=None, index=True)
 
 
-class HeroBase(SQLModel):
-    name: str = Field(index=True)
-    age: int = Field(default=None, index=True)
-
-
-class Hero(HeroBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    secret_name: str
-
-
-class HeroPublic(HeroBase):
-    id: int
-
-
-class HeroCreate(HeroBase):
-    secret_name: str
-
-
-class HeroUpdate(HeroBase):
-    name: str | None = None
-    age: str | None = None
-    secret_name: str | None = None
-
-
