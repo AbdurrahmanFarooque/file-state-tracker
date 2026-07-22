b7281b45945dedd24edf9fca18efe0ef4e71b08f
Abdurrahman Farooque <108559735+AbdurrahmanFarooque@users.noreply.github.com>
2026-07-22 13:01:33 +0530

    Postgres communication (#3)
    * Forgot to merge last time
    
    * implemented all API end-points

--- database.py
+++ database.py
@@ -1,12 +1,15 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import select, create_engine, SQLModel, Session
+
+from models import FileState
 
 
 postgres_url = "postgresql://postgres:1234@localhost:5432/postgres"
 
-engine = create_engine(postgres_url)
+connect_args = {"check_same_thread": False}
+engine = create_engine(postgres_url, echo=True, connect_args=connect_args) # Remove echo=True in production
 
 
 def create_db_and_tables():
@@ -16,5 +19,27 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+def populate_filestate():
+    with Session(engine) as session:
+        filestate_is_empty = session.exec(select(FileState)).first() is None
+
+        if filestate_is_empty:
+            filestate_filing = FileState(label="filing")
+            filestate_section = FileState(label="section")
+            filestate_listing = FileState(label="listing")
+            filestate_court = FileState(label="court")
+            filestate_chamber = FileState(label="chamber")
+            filestate_records = FileState(label="records")
+
+            session.add(filestate_filing)
+            session.add(filestate_section)
+            session.add(filestate_listing)
+            session.add(filestate_court)
+            session.add(filestate_chamber)
+            session.add(filestate_records)
+            session.commit()
+
 
 SessionDependancy = Annotated[Session, Depends(get_session)]
--- models.py
+++ models.py
@@ -1,9 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseModel
 from sqlmodel import SQLModel, Field
 
 
+# case_file
 class CaseFile(SQLModel, table=True):
+    cnr: int = Field(primary_key=True)
+    state_id: int | None = Field(default=None, foreign_key="filestate.id")
+
+
+# file_state
+class FileState(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    cnr: int = Field(index=True)
-    state: str = Field(default=None, index=True)
-
-
+    label: str = Field(unique=True)
